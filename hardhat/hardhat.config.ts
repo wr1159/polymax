736dfb122c2d7cdcdcea4d7618f2e67267278aa7
wr1159 <56021409+wr1159@users.noreply.github.com>
2024-01-09 14:40:46 +0700

    Fix HardHat package dependencies

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
+import { config as dotenvConfig } from "dotenv";
 import "@nomicfoundation/hardhat-toolbox";
+
+// Load environment variables from .env file
+dotenvConfig();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [process.env.DEPLOYER_PRIVATE_KEY as string]
+    }
+  }
 };
 
 export default config;
