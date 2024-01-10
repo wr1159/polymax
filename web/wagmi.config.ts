d9efdc54cd07c50918decf110311f39979dbd94e
wr1159 <56021409+wr1159@users.noreply.github.com>
2024-01-10 23:03:17 +0700

    Fix type error

--- web/wagmi.config.ts
+++ web/wagmi.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "@wagmi/cli"
 import { react } from "@wagmi/cli/plugins"
 import { abi } from "./data/DepositWithdrawABI"
+import { Abi } from "viem"
 
 export default defineConfig({
   out: "lib/generated/blockchain.ts",
   contracts: [
     {
       name: "DepositWithdraw",
-      abi: abi,
+      abi: abi as Abi,
     },
   ],
   plugins: [react()],
